4ae3fa5a8f1fad71327ad5363e276b57fc2e3b0f
Om Raja <omraja451@gmail.com>
2025-11-26 00:06:59 +0530

    fix: add game over if snake crosses boundary and fix snake movement

--- app.js
+++ app.js
@@ -3,29 +3,29 @@ const noOfRows = Math.floor(board.clientHeight / 30);
 const noOfCols = Math.floor(board.clientWidth / 30);
 
 const listOfCells = [];
-const snake = [{ row: 1, col: 2 }];
+const snake = [{ row: 1, col: 2 }, { row: 1, col: 3 }, { row: 1, col: 4 }];
 let direction = "right";
 let food;
 
 //Choosing direction
-window.addEventListener("keydown", (event)=>{
-    switch(event.key){
-        case "ArrowUp":
-            direction = "up";
-            break;
-        case "ArrowDown":
-            direction = "down";
-            break;
-        case "ArrowLeft":
-            direction = "left";
-            break;
-        case "ArrowRight":
-            direction = "right";
-            break;
-        default:
-            direction = "right";
-    }
-})
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      direction = "up";
+      break;
+    case "ArrowDown":
+      direction = "down";
+      break;
+    case "ArrowLeft":
+      direction = "left";
+      break;
+    case "ArrowRight":
+      direction = "right";
+      break;
+    default:
+      direction = "right";
+  }
+});
 
 for (let i = 0; i < noOfRows; i++) {
   for (let j = 0; j < noOfCols; j++) {
@@ -42,49 +42,83 @@ function buildSnake() {
   }
 }
 
-function renderFood(){
-    food = {row: Math.floor(Math.random()*noOfRows), col: Math.floor(Math.random()*noOfCols)};
-    
-    listOfCells[`${food.row}-${food.col}`].classList.add("food");
+function renderFood() {
+  food = {
+    row: Math.floor(Math.random() * noOfRows),
+    col: Math.floor(Math.random() * noOfCols),
+  };
+
+  listOfCells[`${food.row}-${food.col}`].classList.add("food");
 }
+
+
 
 function runSnake() {
   switch (direction) {
     case "right":
-      for (let i = 0; i < snake.length; i++) {
-        listOfCells[`${snake[i].row}-${snake[i].col}`].classList.remove("snakeCell");
-        snake[i].col += 1;
+      for (let i = snake.length - 1; i > 0; i--) {
+        listOfCells[`${snake[i].row}-${snake[i].col}`].classList.remove(
+          "snakeCell",
+        );
+        snake[i] = snake[i - 1];
       }
+      listOfCells[`${snake[0].row}-${snake[0].col}`].classList.remove(
+        "snakeCell",
+      );
+      snake[0].col += 1;
+      if(snake[0].col == noOfCols) endGame();
       buildSnake();
       break;
     case "left":
-      for (let i = 0; i < snake.length; i++) {
-        listOfCells[`${snake[i].row}-${snake[i].col}`].classList.remove("snakeCell");
-        snake[i].col -= 1;
+      for (let i = snake.length - 1; i > 0; i--) {
+        listOfCells[`${snake[i].row}-${snake[i].col}`].classList.remove(
+          "snakeCell",
+        );
+        snake[i] = snake[i - 1];
       }
+      listOfCells[`${snake[0].row}-${snake[0].col}`].classList.remove(
+        "snakeCell",
+      );
+      snake[0].col -= 1;
+      if(snake[0].col == -1) endGame();
       buildSnake();
       break;
     case "up":
-      for (let i = 0; i < snake.length; i++) {
-        listOfCells[`${snake[i].row}-${snake[i].col}`].classList.remove("snakeCell");
-        snake[i].row -= 1;
+      for (let i = snake.length - 1; i > 0; i--) {
+        listOfCells[`${snake[i].row}-${snake[i].col}`].classList.remove(
+          "snakeCell",
+        );
+        snake[i] = snake[i - 1];
       }
+      listOfCells[`${snake[0].row}-${snake[0].col}`].classList.remove(
+        "snakeCell",
+      );
+      snake[0].row -= 1;
+      if(snake[0].row == -1) endGame();
       buildSnake();
       break;
     case "down":
-      for (let i = 0; i < snake.length; i++) {
-        listOfCells[`${snake[i].row}-${snake[i].col}`].classList.remove("snakeCell");
-        snake[i].row += 1;
+      for (let i = snake.length - 1; i > 0; i--) {
+        listOfCells[`${snake[i].row}-${snake[i].col}`].classList.remove(
+          "snakeCell",
+        );
+        snake[i] = snake[i - 1];
       }
+      listOfCells[`${snake[0].row}-${snake[0].col}`].classList.remove(
+        "snakeCell",
+      );
+      snake[0].row += 1;
+      if(snake[0].row == noOfRows) endGame();
       buildSnake();
       break;
   }
 }
 
-const intervalId = setInterval(()=>{
-    runSnake();
+const intervalId = setInterval(() => {
+  runSnake();
 }, 450);
 
-setTimeout(()=>{
-    // clearInterval(intervalId);
-}, 2000);
+function endGame(){
+    clearInterval(intervalId);
+    return alert("Game Over!");
+}
